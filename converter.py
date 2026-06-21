c2092f463946bac73171559372c08e6f4ff82bee
pranavmohantyy <mohantypranav147@gmail.com>
2026-06-21 14:21:39 -0700

    add length conversion functions

---
+++ converter.py
@@ -0,0 +1,33 @@
+def meters_to_feet(meters):
+    return meters * 3.28084
+
+def feet_to_meters(feet):
+    return feet / 3.28084
+
+
+def meters_to_inches(meters):
+    return meters * 39.3701
+
+def inches_to_meters(inches):
+    return inches / 39.3701
+
+
+def meters_to_cm(meters):
+    return meters * 100
+
+def cm_to_meters(cm):
+    return cm / 100
+
+
+def meters_to_km(meters):
+    return meters / 1000
+
+def km_to_meters(km):
+    return km * 1000
+
+
+def meters_to_miles(meters):
+    return meters / 1609.34
+
+def miles_to_meters(miles):
+    return miles * 1609.34
